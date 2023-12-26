9b5fc2363f7e25fb4ce34b8e8c968b77baf9fd78
umesh bhatiya <umesh.bhatiya143@gmail.com>
2023-12-26 22:31:08 +0530

    fixed bugs

---
+++ src/pages/ForgotPassword.js
@@ -0,0 +1,12 @@
+// pages/ForgotPassword.js
+import React from 'react';
+
+const ForgotPassword = () => {
+  return (
+    <div>
+      Your ForgotPassword page content
+    </div>
+  );
+};
+
+export default ForgotPassword;
